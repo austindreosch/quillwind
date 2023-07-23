cbd851e4edcec86852f943ce5670eea32075de58
Austin Dreosch <austindreosch@gmail.com>
2023-07-22 19:09:36 -0700

    implenting openai functionality

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,29 +1,50 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { Configuration, OpenAIApi } from "openai"
+import { Configuration, OpenAIApi } from "openai";
 
 export default async function handler(req, res) {
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
     })
     const openai = new OpenAIApi(configuration)
+    const { topic, keywords } = req.body;
 
-    const topic = "Top 10 tips for dog owners"
-    const keywords = "first time dog owner, common dog health issues, cutest dog breeds"
+    // const response = await openai.createCompletion({
+    //     // model: "text-davinci-003",
+    //     model: "gpt-3.5-turbo",
+    //     temperature: 0.5,
+    //     max_tokens: 3600,
+    //     prompt: `Write a long and detailed SEO-friendly blog post about ${topic}. that targets the following comma-separated keywords: ${keywords}. The content should be formatted in SEO-minded HTML, and the response must also include appropriate HTML, title, and meta description content. The return format must be stringified JSON in the following format:
+    //     {
+    //         "title": title here,
+    //         "metaDescription": meta description here,
+    //         "postContent": post content here,
+    //     }
+    //     `,
+    // })
 
-    // model: "gpt-3.5-turbo",
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        temperature: 0.5,
-        max_tokens: 3600,
-        prompt: `Write a long and detailed SEO-friendly blog post about ${topic}. that targets the following comma-separated keywords: ${keywords}. The content should be formatted in SEO-minded HTML, and the response must also include appropriate HTML, title, and meta description content. The return format must be stringified JSON in the following format:
-        {
-            "postContent": post content here,
-            "title": title here,
-            "metaDescription": meta description here,
-        }
-        `,
+    const postContentResponse = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        temperature: 0.3,
+        // max_tokens: 3600,
+        messages: [{
+            role: "system",
+            content: "You are a blog post generator."
+            }, {
+                role: "user",
+                content: `Write a long and detailed SEO-friendly blog post about ${topic}. that targets the following comma-separated keywords: ${keywords}. The content should be formatted in SEO-minded HTML, limited to the following HTML tags: p, h1, h2, h3, h4, h5, h6, strong, ul, ol, li, i, img, and blockquote.`
+            }
+        ]
     })
 
-    res.status(200).json({ post: response.data.choices[0].text })
+    // console.log("response", postContentResponse.data)
+    console.log("response", postContentResponse.data.choices[0]?.message?.content);
+
+    try {
+        const post = JSON.parse(postContentResponse.data.choices[0]?.text.split("\n").join(""));
+        res.status(200).json({ post: post })
+    } catch (error) {
+        console.log('error on json parse:', error);
+        res.status(500).json({ error: 'Error parsing the OpenAI response.' })
+    }
   }
   
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,22 +1,65 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import axios from "axios";
+import { useState } from "react";
+import { Comment } from 'react-loader-spinner';
 import { AppLayout } from "../../components/AppLayout";
 
 export default function NewPost(props) {
-    console.log("This is the new post props.", props);
+    // console.log("This is the new post props.", props);
+    const [postContent, setPostContent ] = useState("");
+    const [topic, setTopic] = useState("");
+    const [keywords, setKeywords] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
-    const handleClick = async () => {
-        const reponse = fetch('/api/generatePost', {
-            method: 'POST',
-        });
-        const json = await reponse.json();
-        console.log("Result:", json);
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+        // console.log("we are loading new post");
+        try{
+            const response = await axios.post('/api/generatePost', {topic, keywords}, {
+                headers: {
+                    'content-type': 'application/json',
+                }
+            });
+            console.log('Response data:', response.data); 
+            setPostContent(response.data.post.postContent);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            // console.log("we are done loading new post");
+            setIsLoading(false);
+        }
     }
 
     return (
         <div>
-            <button className="text-my-white bg-my-lightblue w-64 text-center cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-my-white hover:text-my-black transition-colors block text-heading-500 tracking-widest my-4 mx-auto">
-                <a href="/api/generatePost">Generate</a>
-            </button>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        <strong>Generate a blog post on the topic of:</strong>
+                    </label>
+                    <textarea  className="resize-none border border-my-black w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={(evt) => setTopic(evt.target.value)}></textarea>
+                </div>
+                <div>
+                    <label>
+                        <strong>Targeting the following keywords: </strong>
+                    </label>
+                    <textarea className="resize-none border border-my-black w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={(evt) => setKeywords(evt.target.value)}></textarea>
+                </div>
+                <button type="submit" className="text-my-white bg-my-lightblue w-64 text-center cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-my-white hover:text-my-black transition-colors block text-heading-500 tracking-widest my-4 mx-auto">
+                    Generate
+                </button>
+            </form>
+            {isLoading ? (
+                <div className="fixed inset-0 flex items-center justify-center pl-80">
+                    <Comment visible={true} height="80" width="80" ariaLabel="comment-loading" wrapperStyle={{}} wrapperClass="comment-wrapper" color="#fff" backgroundColor="#00a8e8ff" />
+                </div>
+                ) : null}
+                {!isLoading && postContent && (
+                <div>
+                    <div dangerouslySetInnerHTML={{ __html: postContent }} />
+                </div>
+                )}
         </div>
     )
 }
