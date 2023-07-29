e021c0ea361a87c6a554b699f75f6422d4e21f01
Austin Dreosch <austindreosch@gmail.com>
2023-07-28 22:46:20 -0700

    added offpage styles

--- pages/purchase.js
+++ pages/purchase.js
@@ -1,4 +1,6 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { faSackDollar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import { AppLayout } from "../components/AppLayout";
 import { getAppProps } from "../utils/getAppProps";
@@ -14,9 +16,17 @@ export default function BuyBucks() {
   }  
 
     return (
-      <div>
-        <h1>Quillbucks Dashboard</h1>
-        <button onClick={handleClick} className="text-my-white bg-my-green w-64 text-center cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-my-white hover:text-my-black transition-colors block text-heading-500 tracking-widest my-4 mx-auto">Add Quillbucks</button>
+
+      <div className="flex items-center justify-center min-h-screen p-12">
+        <div className="w-[550px] h-[320px] max-w-[550px] rounded-lg bg-gray-100 shadow-lg text-center">
+          <div className="flex items-center justify-center">
+            <div className="text-5xl text-center font-heading pb-4 font-semibold text-my-green ml-2">
+            <FontAwesomeIcon icon={faSackDollar} className="text-my-yellow mx-auto mt-20 pb-2 px-2" size="2x"/>  
+                5
+            </div>
+          </div>
+          <button onClick={handleClick} className="text-my-white bg-my-green w-64 text-center cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-my-white hover:text-my-black transition-colors block text-heading-500 tracking-widest my-4 mx-auto">Add Quillbucks</button>
+        </div>
       </div>
     )
   
--- pages/success.js
+++ pages/success.js
@@ -1,13 +1,22 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { faBagShopping } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import Link from "next/link";
 import { AppLayout } from "../components/AppLayout";
 import { getAppProps } from "../utils/getAppProps";
 
 export default function Success() {
 
     return (
-      <div>
-        <h1>Thank you so much for your purchase!</h1>
+      <div className="flex items-center justify-center min-h-screen p-12">
+        <div className="w-[550px] h-[320px] max-w-[550px] rounded-lg bg-gray-100 shadow-lg text-center">
+          <FontAwesomeIcon icon={faBagShopping} className="text-my-green mx-auto mt-20 pb-2 " size="4x"/>
+          <div className="text-3xl text-center pb-4 font-body-600 text-my-darkblue">
+              Thank you for your purchase!
+          </div>
+          <Link href="/post/new" className="text-my-white bg-my-lightblue w-64 text-center cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-my-white hover:text-my-black transition-colors block text-heading-500 tracking-widest mx-auto">New Post</Link>
+        </div>
       </div>
     )
   
